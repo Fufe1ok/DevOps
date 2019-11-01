3598810fe3adb97a93096e839aefca03ad62a906
Fufe1ok <32967290+Fufe1ok@users.noreply.github.com>
2019-11-01 12:14:23 +0200

    Update LocationController.java

--- lab2/back-end/src/main/java/test/vakoms/presentation/LocationController.java
+++ lab2/back-end/src/main/java/test/vakoms/presentation/LocationController.java
@@ -18,7 +18,7 @@ import java.util.Set;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
-@CrossOrigin(origins = "https://lab2front-256619.appspot.com")
+@CrossOrigin(origins = "https://lab2-frontend.appspot.com")
 @RestController
 public class LocationController {
 
