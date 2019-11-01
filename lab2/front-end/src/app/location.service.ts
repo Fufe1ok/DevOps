53a5c53c4d14b2aa4542bb09925e4cc269dc9c9a
Fufe1ok <32967290+Fufe1ok@users.noreply.github.com>
2019-11-01 12:28:46 +0200

    Update location.service.ts

--- lab2/front-end/src/app/location.service.ts
+++ lab2/front-end/src/app/location.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class LocationService {
 
-  private baseUrl = 'https://lab2-256619.appspot.com';
+  private baseUrl = 'https://lab2-backend.appspot.com';
 
   constructor(private http: HttpClient) { }
 
